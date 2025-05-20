c96ff91f98469688c0be6721a1c78129b8325228
Toni Díaz <tonidiazfdez@gmail.com>
2025-05-21 01:29:28 +0200

    V1.0.1: creamos la ruta de inicio y su html

---
+++ src/app.py
@@ -0,0 +1,12 @@
+from flask import flask, request, jsonify, render_template, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+
+# Conexión a la base de datos
+db = MySQL(app)
+
+@app.route('/')
+def index():
+    return render_template('inicio.html')
